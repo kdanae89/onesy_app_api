741d6e0d5e5caf528dee1ea5a4dcf736ed3b06eb
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-02-15 12:30:32 -0800

    set up cors for heroku

---
+++ db/migrate/20170214232845_remove_ledgers_qty_again.rb
@@ -0,0 +1,5 @@
+class RemoveLedgersQtyAgain < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :ledgers, :qty
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209163745) do
+ActiveRecord::Schema.define(version: 20170214232845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,12 +19,12 @@ ActiveRecord::Schema.define(version: 20170209163745) do
     t.string   "img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "image_id"
   end
 
   create_table "ledgers", force: :cascade do |t|
     t.integer  "image_id"
     t.integer  "onesy_id"
-    t.integer  "qty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["image_id"], name: "index_ledgers_on_image_id", using: :btree
