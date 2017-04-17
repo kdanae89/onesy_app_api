46c0971bc4eb24145165ec8a989c8b93679045b6
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-04-16 21:31:57 -0700

    trying to deploy to heroku

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -2,7 +2,7 @@ class Ledger < ApplicationRecord
   belongs_to :image
   belongs_to :onesy
 
-  validates_uniqueness_of: :image_id, scope: :onesy_id
-  validates_uniqueness_of: :onesy_id, scope: :image_id
+  validates_uniqueness_of :image_id, scope: :onesy_id
+  validates_uniqueness_of :onesy_id, scope: :image_id
   end
 end
