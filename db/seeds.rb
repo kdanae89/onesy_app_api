aaf12b75f739ac61d1187ba489ebe29e442c7b8b
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-04-20 15:52:30 -0700

    took out images throwing errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Image.create(
-img:"http://i918.photobucket.com/albums/ad27/Mjk97/D100.jpg"
-)
-
-Image.create(
-img:"http://i40.tinypic.com/rwtp9t.jpg"
-)
-
-Image.create(
-img:"http://orig09.deviantart.net/203b/f/2011/159/c/b/stitch_icon_1___100x100_by_chidori1334-d3ice3s.png"
-)
-
-Image.create(
-img:"http://i42.tinypic.com/bim8tf.jpg"
-)
+# Image.create(
+# img:"http://i918.photobucket.com/albums/ad27/Mjk97/D100.jpg"
+# )
+#
+# Image.create(
+# img:"http://i40.tinypic.com/rwtp9t.jpg"
+# )
+#
+# Image.create(
+# img:"http://orig09.deviantart.net/203b/f/2011/159/c/b/stitch_icon_1___100x100_by_chidori1334-d3ice3s.png"
+# )
+#
+# Image.create(
+# img:"http://i42.tinypic.com/bim8tf.jpg"
+# )
 
 Image.create(
 img:"https://66.media.tumblr.com/tumblr_ma1f5420fM1rwyed6.png"
@@ -30,17 +30,17 @@ Image.create(
 img:"https://www.roleplaygateway.com/download/file.php?avatar=77610_1445167405.png"
 )
 
-Image.create(
-img:"http://orig01.deviantart.net/3177/f/2011/067/3/4/vegeta_icon_by_meteora94-d3b703j.png"
-)
-
-Image.create(
-img:"http://forum.darkspyro.net/upload/iY3mZ01ysmGLlK2E1CTx.png"
-)
-
-Image.create(
-img:"http://piq.codeus.net/static/media/userpics/piq_4652.png"
-)
+# Image.create(
+# img:"http://orig01.deviantart.net/3177/f/2011/067/3/4/vegeta_icon_by_meteora94-d3b703j.png"
+# )
+#
+# Image.create(
+# img:"http://forum.darkspyro.net/upload/iY3mZ01ysmGLlK2E1CTx.png"
+# )
+#
+# Image.create(
+# img:"http://piq.codeus.net/static/media/userpics/piq_4652.png"
+# )
 
 Image.create(
 img:"https://screenshots.en.sftcdn.net/en/scrn/3345000/3345450/the-lord-of-the-rings-online-01-100x100.png"
@@ -50,89 +50,89 @@ Image.create(
 img:"https://image.spreadshirtmedia.com/image-server/v1/compositions/4164868/views/1,width=100,height=100,appearanceId=1,version=1478496810/lord-of-the-rings-t-shirt-men-s-t-shirt.jpg"
 )
 
-Image.create(
-img:"http://avatarbox.net/avatars/img28/pokemon_smile_avatar_picture_83232.jpg"
-)
-
-Image.create(
-img:"http://thenewswheel.com/wp-content/uploads/2016/08/Slowpoke-Pokemon-100x100.png"
-)
+# Image.create(
+# img:"http://avatarbox.net/avatars/img28/pokemon_smile_avatar_picture_83232.jpg"
+# )
+#
+# Image.create(
+# img:"http://thenewswheel.com/wp-content/uploads/2016/08/Slowpoke-Pokemon-100x100.png"
+# )
 
 Image.create(
 img:"https://image.spreadshirtmedia.com/image-server/v1/compositions/111138014/views/1,width=100,height=100,appearanceId=1,version=1473664654/golden-chocobo-men-s-t-shirt-men-s-premium-t-shirt.jpg"
 )
 
-Image.create(
-img:"http://piq.codeus.net/static/media/userpics/piq_119903.png"
-)
-
-Image.create(
-img:"http://www.godisageek.com/wp-content/uploads/Final-Fantasy-X-100x100.jpg"
-)
+# Image.create(
+# img:"http://piq.codeus.net/static/media/userpics/piq_119903.png"
+# )
+#
+# Image.create(
+# img:"http://www.godisageek.com/wp-content/uploads/Final-Fantasy-X-100x100.jpg"
+# )
 
 Image.create(
 img:"https://images-na.ssl-images-amazon.com/images/M/MV5BMTk2MzI0ODc1N15BMl5BanBnXkFtZTcwMTI5OTEyNw@@._V1._SX100_SY100_.jpg"
 )
 
-Image.create(
-img:"http://images6.fanpop.com/image/photos/35300000/Howl-s-Moving-Castle-howls-moving-castle-35399437-100-100.png"
-)
-
-Image.create(
-img:"http://piq.codeus.net/static/media/userpics/piq_63969.png"
-)
-
-Image.create(
-img:"http://www.emoticonswallpapers.com/avatar/games/Super-Mario-waits.gif"
-)
+# Image.create(
+# img:"http://images6.fanpop.com/image/photos/35300000/Howl-s-Moving-Castle-howls-moving-castle-35399437-100-100.png"
+# )
+#
+# Image.create(
+# img:"http://piq.codeus.net/static/media/userpics/piq_63969.png"
+# )
+#
+# Image.create(
+# img:"http://www.emoticonswallpapers.com/avatar/games/Super-Mario-waits.gif"
+# )
 
 Image.create(
 img:"https://i.downloadatoz.com/download/icon2/b/b/5/5378beef3c3f45d92fb612f15377fe1a.jpg"
 )
 
-Image.create(
-img:"http://avatarbox.net/avatars/img11/batman_symbol_avatar_picture_84003.jpg"
-)
-
-Image.create(
-img:"http://i.gr-assets.com/images/S/compressed.photo.goodreads.com/users/1472174020i/58987941._UY100_CR20,0,100,100_.jpg"
-)
-
-Image.create(
-img:"http://is5.mzstatic.com/image/thumb/Purple22/v4/3a/94/ec/3a94ec97-7e7b-bc34-c34b-65464989a816/source/100x100bb.jpg"
-)
-
-Image.create(
-img:"http://blogs.plos.org/speakeasyscience/files/2012/05/poison.jpg-100x100.png"
-)
-
-Image.create(
-img:"http://ll-us-i5.wal.co/dfw/4ff9c6c9-746e/k2-_178606d4-beb0-484a-bc1b-8e41d4143547.v1.png-6450307b2a9ffb64435f2c8b802d668854521322-crushed-100x100.png"
-)
-
-Image.create(
-img:"http://media-minecraftforum.cursecdn.com/avatars/557/952/635623582159893766.png"
-)
-
-Image.create(
-img:"http://images1.wikia.nocookie.net/__cb20130825080654/cybermon/images/2/24/Reindeer.png"
-)
-
-Image.create(
-img:"http://www.dumbbum.net/freegameavatars/zelda/The_Legend_of_Zelda_the_Wind_Waker_Link_1.png"
-)
-
-Image.create(
-img:"http://avatarbox.net/avatars/img10/zelda_triforce_avatar_picture_82097.png"
-)
-
-Image.create(
-img:"http://images5.fanpop.com/image/photos/31500000/Link-the-legend-of-zelda-31540052-100-100.jpg"
-)
-
-Image.create(
-img:"http://www.vanilladome.fr/perso/232/003.png"
-)
+# Image.create(
+# img:"http://avatarbox.net/avatars/img11/batman_symbol_avatar_picture_84003.jpg"
+# )
+#
+# Image.create(
+# img:"http://i.gr-assets.com/images/S/compressed.photo.goodreads.com/users/1472174020i/58987941._UY100_CR20,0,100,100_.jpg"
+# )
+#
+# Image.create(
+# img:"http://is5.mzstatic.com/image/thumb/Purple22/v4/3a/94/ec/3a94ec97-7e7b-bc34-c34b-65464989a816/source/100x100bb.jpg"
+# )
+#
+# Image.create(
+# img:"http://blogs.plos.org/speakeasyscience/files/2012/05/poison.jpg-100x100.png"
+# )
+#
+# Image.create(
+# img:"http://ll-us-i5.wal.co/dfw/4ff9c6c9-746e/k2-_178606d4-beb0-484a-bc1b-8e41d4143547.v1.png-6450307b2a9ffb64435f2c8b802d668854521322-crushed-100x100.png"
+# )
+#
+# Image.create(
+# img:"http://media-minecraftforum.cursecdn.com/avatars/557/952/635623582159893766.png"
+# )
+#
+# Image.create(
+# img:"http://images1.wikia.nocookie.net/__cb20130825080654/cybermon/images/2/24/Reindeer.png"
+# )
+#
+# Image.create(
+# img:"http://www.dumbbum.net/freegameavatars/zelda/The_Legend_of_Zelda_the_Wind_Waker_Link_1.png"
+# )
+#
+# Image.create(
+# img:"http://avatarbox.net/avatars/img10/zelda_triforce_avatar_picture_82097.png"
+# )
+#
+# Image.create(
+# img:"http://images5.fanpop.com/image/photos/31500000/Link-the-legend-of-zelda-31540052-100-100.jpg"
+# )
+#
+# Image.create(
+# img:"http://www.vanilladome.fr/perso/232/003.png"
+# )
 
 Image.create(
 img:"https://www.mariowiki.com/images/a/a9/047-SMMGoomba.png"
@@ -150,30 +150,30 @@ Image.create(
 img:"https://www.sonicstadium.org/wp-content/uploads/2009/02/concept-sonic2-100x100.png"
 )
 
-Image.create(
-img:"http://www.coloring4all.com/coloring/NinjaTurtlesRaphael.png"
-)
-
-Image.create(
-img:"http://static.kiabi.es/images/brands/TeenageMutantNinjaTurtles.png"
-)
-
-Image.create(
-img:"http://community.playstarbound.com/data/attachments/32/32424-544c75d63bad39b5758c3feda01d5632.jpg"
-)
-
-Image.create(
-img:"http://www.emoticonswallpapers.com/avatar/games/Mega-Man-face.png"
-)
+# Image.create(
+# img:"http://www.coloring4all.com/coloring/NinjaTurtlesRaphael.png"
+# )
+#
+# Image.create(
+# img:"http://static.kiabi.es/images/brands/TeenageMutantNinjaTurtles.png"
+# )
+#
+# Image.create(
+# img:"http://community.playstarbound.com/data/attachments/32/32424-544c75d63bad39b5758c3feda01d5632.jpg"
+# )
+#
+# Image.create(
+# img:"http://www.emoticonswallpapers.com/avatar/games/Mega-Man-face.png"
+# )
 
 Image.create(
 img:"https://imagenes.es.sftcdn.net/es/scrn/90000/90310/bombermaaan-10-100x100.png"
 )
 
-Image.create(
-img:"http://piq.codeus.net/static/media/userpics/piq_3921.png"
-)
-
-Image.create(
-img:"http://dragonballsuper.info/wp-content/uploads/2016/08/Goku-3-100x100.png"
-)
+# Image.create(
+# img:"http://piq.codeus.net/static/media/userpics/piq_3921.png"
+# )
+#
+# Image.create(
+# img:"http://dragonballsuper.info/wp-content/uploads/2016/08/Goku-3-100x100.png"
+# )
